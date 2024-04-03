d8b492148a740ac0209dede74c70a982527f5a48
Aditya Singh <124818450+Aditya1464@users.noreply.github.com>
2024-04-03 14:43:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0079-word-search/0079-word-search.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+
+    bool isValid(vector<vector<char>>& board, string word, vector<vector<int>> visit, int i, int j){
+        if(i >= board.size() || j >= board[0].size() || i < 0 || j < 0) return false;
+        if(visit[i][j] == 1) return false;
+        if(board[i][j] != word[0]) return false;
+
+        return true;
+    }
+
+    bool helper(vector<vector<char>>& board, string word, vector<vector<int>> visit, int i, int j){
+
+        if(word.size() == 0){
+            return true;
+        }
+
+        visit[i][j] = 1;
+
+        bool isTrue = false;
+
+        //right
+        if(isValid(board, word, visit, i, j+1)){
+            isTrue = helper(board, word.substr(1), visit, i, j+1);
+            if(isTrue) return true;
+        }
+
+        // left
+        if(isValid(board, word, visit, i, j-1)){
+            isTrue = helper(board, word.substr(1), visit, i, j-1);
+            if(isTrue) return true;
+        }
+
+        //up
+        if(isValid(board, word, visit, i-1, j)){
+            isTrue = helper(board, word.substr(1), visit, i-1, j);
+            if(isTrue) return true;
+        }
+
+        // down
+        if(isValid(board, word, visit, i+1, j)){
+            isTrue = helper(board, word.substr(1), visit, i+1, j);
+            if(isTrue) return true;
+        }
+
+        visit[i][j] = 0;
+        return isTrue;
+    }
+
+    bool exist(vector<vector<char>>& board, string word) {
+        int m = board.size();
+        int n = board[0].size();
+        vector<vector<int>> visit(m, vector<int>(n, 0));
+
+
+        bool isTrue = false;
+        for(int i=0; i<board.size(); i++){
+            for(int j=0; j<board[i].size(); j++){
+                if(board[i][j] == word[0]){
+                    isTrue = helper(board, word.substr(1), visit, i, j);
+                }
+
+                if(isTrue) return true;
+            }
+        }
+
+        return isTrue;
+    }
+};
