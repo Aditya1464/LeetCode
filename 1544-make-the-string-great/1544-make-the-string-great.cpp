edb1aa698791a6be53e319c1ce795a8771e10c64
Aditya Singh <124818450+Aditya1464@users.noreply.github.com>
2024-04-05 14:09:04 +0530

    Time: 0 ms (100%), Space: 7.4 MB (99.85%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,37 +1,17 @@
 class Solution {
 public:
-
-    // void helper(string& s, int i){
-    //     if(i == s.size()) return;
-    //     if(i > s.size() - 2) return;
-
-    //     if(s[i] == s[i+1] + 32 || s[i] + 32 == s[i+1]){
-    //         s.erase(i, 2);
-    //         helper(s, 0);
-    //         return;
-    //     }
-
-    //     helper(s, i+1);
-    //     return;
-    // }
-
-    string makeGood(string s) {
-        
-        // helper(s, 0);
-        // return s;
+    string makeGood(string s) {        
         int i=0;
         while(i < s.size()){
             if(i > s.size()-2) break;
-
             if(s[i] == s[i+1] + 32 || s[i] + 32 == s[i+1]){
                 s.erase(i, 2);
-                i = 0;
+                if(i==0) i = 0;
+                else i--;
                 continue;
             }
-
             i++;
         }
-
         return s;
     }
 };
