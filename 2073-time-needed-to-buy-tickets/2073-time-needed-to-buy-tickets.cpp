0d3d1437ec81e9b63b356bd041010e7ad234d385
Aditya Singh <124818450+Aditya1464@users.noreply.github.com>
2024-04-09 13:15:35 +0530

    Time: 11 ms (13.43%), Space: 9.3 MB (67.56%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -2,27 +2,48 @@ class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
         
-        queue<int> q;
-        for(int i=0; i<tickets.size(); i++){
-            q.push(tickets[i]);
-        }
+        // queue<int> q;
+        // for(int i=0; i<tickets.size(); i++){
+        //     q.push(tickets[i]);
+        // }
+
+        // int ans = 0;
+
+        // while(!q.empty()){
+
+        //     int top = q.front();
+        //     top--;
+        //     ans++;
+
+        //     if(k == 0 && top == 0){
+        //         return ans;
+        //     }
+
+        //     q.pop();
+        //     if(top != 0) q.push(top);
+            
+        //     if(k == 0) k = q.size() - 1;
+        //     else k--;
+        // }
+
+        // return ans;
+
+
 
         int ans = 0;
-
-        while(!q.empty()){
-
-            int top = q.front();
+        while(tickets.size() != 0){
+            int top = tickets[0];
+            tickets.erase(tickets.begin());
             top--;
             ans++;
 
-            if(k == 0 && top == 0){
+            if(k==0 && top == 0){
                 return ans;
             }
 
-            q.pop();
-            if(top != 0) q.push(top);
-            
-            if(k == 0) k = q.size() - 1;
+            if(top != 0) tickets.push_back(top);
+
+            if(k == 0) k = tickets.size() - 1;
             else k--;
         }
 
