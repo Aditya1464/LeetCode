8bc3aa2f2d66e404a43e1412f957d9b866af795c
Aditya Singh <124818450+Aditya1464@users.noreply.github.com>
2024-04-19 13:58:11 +0530

    Time: 23 ms (90.28%), Space: 15.8 MB (81.63%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
-    bool isValid(vector<vector<char>>& grid, vector<vector<int>>& visit, int i, int j){
-        return i >= 0 && j >= 0 && i < visit.size() && j < visit[0].size() && visit[i][j] == 0 && grid[i][j] == '1';
+    bool isValid(vector<vector<char>>& grid, int i, int j){
+        return i >= 0 && j >= 0 && i < grid.size() && j < grid[0].size() && grid[i][j] == '1';
     }
 
-    void helper(vector<vector<char>>& grid, vector<vector<int>>& visit, int i, int j){
+    void helper(vector<vector<char>>& grid, int i, int j){
 
-        if(!isValid(grid, visit, i, j)) return;
-        visit[i][j] = 1;
+        if(!isValid(grid, i, j)) return;
+        grid[i][j] = '0';
 
-        helper(grid, visit, i, j+1);
-        helper(grid, visit, i, j-1);
-        helper(grid, visit, i+1, j);
-        helper(grid, visit, i-1, j);
+        helper(grid, i, j+1);
+        helper(grid, i, j-1);
+        helper(grid, i+1, j);
+        helper(grid, i-1, j);
     }
 
     int numIslands(vector<vector<char>>& grid) {
-        
-        vector<vector<int>> visit(grid.size(), vector<int>(grid[0].size(), 0));
-
         int count = 0;
 
         for(int i=0; i<grid.size(); i++){
             for(int j=0; j<grid[i].size(); j++){
-
-                if(grid[i][j] == '1' && visit[i][j] == 0){
-                    helper(grid, visit, i, j);
+                if(grid[i][j] == '1'){
+                    helper(grid, i, j);
                     count ++;
                 }
             }
