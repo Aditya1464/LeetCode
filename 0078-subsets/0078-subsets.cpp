535c6b753854f2f3114d2c3bca0c9f19093ea175
Aditya Singh <124818450+Aditya1464@users.noreply.github.com>
2024-05-21 09:37:18 +0530

    Time: 3 ms (47.78%), Space: 13.6 MB (31.51%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,28 +1,59 @@
 class Solution {
 public:
-    void helper(vector<int>& nums, vector<vector<int>>& subset, int idx){
+    void helper(vector<int> nums, vector<vector<int>>& subset, vector<int>& temp, int idx){
         if(idx == nums.size()){
-            vector<int> temp(0);
             subset.push_back(temp);
             return;
         }
 
-        helper(nums, subset, idx+1);
+        //without
+        helper(nums, subset, temp, idx+1);
 
-        int n = subset.size();
-        for(int i=0; i<n; i++){
-            vector<int> temp = subset[i];
-            temp.push_back(nums[idx]);
-            subset.push_back(temp);
-        }
-
-        return;
+        //with
+        temp.push_back(nums[idx]);
+        helper(nums, subset, temp, idx+1);
+        temp.pop_back();
     }
 
     vector<vector<int>> subsets(vector<int>& nums) {
-        
+        vector<int> temp;
         vector<vector<int>> subset;
-        helper(nums,subset,0);
+        helper(nums, subset, temp, 0);
         return subset;
     }
 };
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     void helper(vector<int>& nums, vector<vector<int>>& subset, int idx){
+//         if(idx == nums.size()){
+//             vector<int> temp(0);
+//             subset.push_back(temp);
+//             return;
+//         }
+
+//         helper(nums, subset, idx+1);
+
+//         int n = subset.size();
+//         for(int i=0; i<n; i++){
+//             vector<int> temp = subset[i];
+//             temp.push_back(nums[idx]);
+//             subset.push_back(temp);
+//         }
+
+//         return;
+//     }
+
+//     vector<vector<int>> subsets(vector<int>& nums) {
+        
+//         vector<vector<int>> subset;
+//         helper(nums,subset,0);
+//         return subset;
+//     }
+// };
