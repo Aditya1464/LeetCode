0e853f1301d3c34cb7c48d21a39a61402004c807
Aditya Singh <124818450+Aditya1464@users.noreply.github.com>
2024-10-16 01:51:43 +0530

    Time: 19 ms (51.12%), Space: 18.7 MB (46.73%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> edges(numCourses);
+        vector<int> inDegree(numCourses, 0);
+
+        for(auto &pre : prerequisites){
+            edges[pre[0]].push_back(pre[1]);
+            inDegree[pre[1]]++;
+        }
+
+        queue<int> q;
+        vector<int> ans;
+
+        for(int i=0; i<numCourses; i++){ 
+            if(inDegree[i] == 0) q.push(i);
+        }
+
+        while(!q.empty()){
+            int front = q.front();
+            ans.push_back(front);
+            q.pop();
+
+            for(auto ed : edges[front]){
+                inDegree[ed]--;
+                if(inDegree[ed] == 0) q.push(ed);
+            }
+        }
+
+        return ans.size() == numCourses;
+    }
+};
