a5096a09b4dff811c4ef2b4851adff8e7e0fc3aa
Aditya Singh <124818450+Aditya1464@users.noreply.github.com>
2025-01-21 17:06:53 +0530

    Time: 4 ms (22.86%), Space: 11.1 MB (35.44%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,39 +1,35 @@
 class Solution {
 public:
     vector<vector<int>> ans;
-    // unordered_set<vector<int>> set;
     void helper(vector<int> nums, int i){
         if(i >= nums.size()){
             ans.push_back({});
             return;
         }
+        
         helper(nums, i+1);
 
         int n = ans.size();
-        for(int j=0; j<n; j++){
-            ans.push_back(ans[j]);
-        }
 
-        for(int j=n; j<ans.size(); j++){
-            ans[j].push_back(nums[i]);
+        for(int j=0; j<n; j++){
+            vector<int> temp = vector<int>(ans[j].begin(), ans[j].end());
+            temp.push_back(nums[i]);
+            sort(temp.begin(), temp.end());
+            
+            bool isTrue = false;
+            for(int k=0; k<ans.size(); k++){
+                if(ans[k] == temp) {
+                    isTrue = true;
+                    break;
+                }
+            }
+
+            if(!isTrue) ans.push_back(temp);
         }
         return;
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         helper(nums, 0);
-        vector<vector<int>> sub;
-
-        for(int i=0; i<ans.size(); i++){
-            bool isTrue = false;
-            sort(ans[i].begin(), ans[i].end());
-            for(int j=0; j<sub.size(); j++){
-                if(sub[j] == ans[i]){
-                    isTrue = true;
-                    break;
-                }
-            }
-            if(!isTrue) sub.push_back(ans[i]);
-        }
-        return sub;
+        return ans;
     }
 };
